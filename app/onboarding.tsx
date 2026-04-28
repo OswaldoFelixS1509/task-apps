106b43a994313c769eb54e3805761eea9f8be152
Oswaldo Felix <felixsanchezoswaldo@gmail.com>
2026-04-27 19:24:49 -0700

    added basic layout

---
+++ app/onboarding.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const Onboarding = () => {
+    return (
+        <View>
+            <Text> 
+                Onboarding
+            </Text>
+        </View>
+
+    )
+}
+ 
+export default Onboarding;
