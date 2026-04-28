6640c2a17103279fa90a95e9445c9c35ddcbaa89
Oswaldo Felix <felixsanchezoswaldo@gmail.com>
2026-04-27 20:16:13 -0700

    Added basic navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,56 @@
-import { Stack } from "expo-router";
+import { tabs } from "@/constants/data";
+import { colors, components } from "@/constants/theme";
+import clsx from "clsx";
+import { Image } from "expo-image";
+import { Tabs } from "expo-router";
+import { View } from 'react-native';
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
+const tabBar = components.tabBar;
 
-export default function RootLayout() {
-  return <Stack screenOptions={{headerShown: false}}/>;
-}
+const TabLayout = () => {
+  const insets = useSafeAreaInsets();
+
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="tabs-icon">
+        <View className={clsx('tabs-pill', focused && 'tabs-active')}>
+          <Image source={icon}
+            className="tabs-glyph"
+          ></Image>
+        </View>
+      </View>
+    );
+  };
+
+  return (
+      <Tabs screenOptions={{ 
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: 'absolute',
+            bottom: Math.max(insets.bottom, tabBar.horizontalInset),
+            height: tabBar.height,
+            marginHorizontal: tabBar.horizontalInset,
+            borderRadius: tabBar.radius,
+            backgroundColor: colors.primary
+          }
+        }}>
+        {tabs.map((tab) => (
+          <Tabs.Screen 
+            name={tab.name}
+            key={tab.name} 
+            options={{
+              title: tab.title,
+              tabBarIcon:({focused}) => (
+                <TabIcon focused={focused} icon={tab.icon}></TabIcon>
+              )
+            }}
+          ></Tabs.Screen>
+        ))}
+    </Tabs>
+  )
+
+};
+
+export default TabLayout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,20 +5,20 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
+      <Text className="text-xl font-bold text-blue-500 text-white">
         Welcome to Nativewind!
       </Text>
-      <Link href='/onboarding' className="mt-4 rounded bg-primary p-4"> Go to onboarding</Link>
+      <Link href='/onboarding' className="mt-4 rounded bg-primary text-white p-4"> Go to onboarding</Link>
 
-      <Link href='/(auth)/sign-in' className="mt-4 rounded bg-primary p-4"> Go to sign in</Link>
+      <Link href='/(auth)/sign-in' className="mt-4 rounded bg-primary text-white p-4"> Go to sign in</Link>
 
-      <Link href='/(auth)/sign-up' className="mt-4 rounded bg-primary p-4"> Go to sign up</Link>
+      <Link href='/(auth)/sign-up' className="mt-4 rounded bg-primary text-white p-4"> Go to sign up</Link>
 
 
 
-      <Link href='/subscriptions/spotify' className="mt-4 rounded bg-primary p-4"> Spotify subs</Link>
+      <Link href='/subscriptions/spotify' className="mt-4 rounded bg-primary text-white p-4"> Spotify subs</Link>
 
-      <Link href='/subscriptions/mpplus' className="mt-4 rounded bg-primary p-4"> Mercado pago +</Link>
+      <Link href='/subscriptions/mpplus' className="mt-4 rounded bg-primary text-white p-4"> Mercado pago +</Link>
     </View>
   );
 }
